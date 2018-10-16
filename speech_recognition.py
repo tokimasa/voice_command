ca97a4402f515934c9880e9d1a8cea76b1d3debb
tokimasa <edwardlai89@gmail.com>
2018-10-16 17:06:00 +0800

    add missing file

---
+++ speech_recognition.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 10:17:21 2018
+
+@author: TOKIMASA
+"""
+
+from preprocess import *
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+
+# Second dimension of the feature is dim2
+feature_dim_2 = 11
+
+# Save data to array file first
+save_data_to_array(max_len=feature_dim_2)
+
+# # Loading train set and test set
+X_train, X_test, y_train, y_test = get_train_test()
+
+# # Feature dimension
+feature_dim_1 = 20
+channel = 1
+epochs = 10
+batch_size = 100
+verbose = 1
+num_classes = 2
+
+# Reshaping to perform 2D convolution
+X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
+
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
+    model.add(Conv2D(48, kernel_size=(2, 2), activation='relu'))
+    model.add(Conv2D(120, kernel_size=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    return model
+
+
+model = get_model()
+model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
+
+from keras.models import load_model
+model.save('model_3.h5')
